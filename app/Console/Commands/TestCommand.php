01536b1dd110a7327d949e6b67f507452ac33181
butterfly026 <butterfly026@github.com>
2024-01-16 12:36:31 -0800

    update test command

--- app/Console/Commands/TestCommand.php
+++ app/Console/Commands/TestCommand.php
@@ -30,7 +30,7 @@ class TestCommand extends Command
      */
     public function handle()
     {
-        Mail::to('drollmatt@outlook.com')->send(new SignupMail(10));
+        Mail::to('drollmatt26@outlook.com')->send(new SignupMail(10));
         return Command::SUCCESS;
     }
 }
