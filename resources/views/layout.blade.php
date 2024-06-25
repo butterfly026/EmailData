3e50d877566b8e29dd0c673f430375ad2ce24418
butterfly026 <butterfly026@github.com>
2024-06-25 00:50:34 -0500

    update google tracking

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -73,6 +73,16 @@
             }).showToast();
         }
     </script>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16623057694">
+    </script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'AW-16623057694');
+    </script>
     <!-- Meta Pixel Code -->
     <script>
         ! function(f, b, e, v, n, t, s) {
